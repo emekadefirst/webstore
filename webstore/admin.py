daa2108469acadc467bdb370ccc9e6663610f5fb
emekadefirst <victorchibuogwu33@gmail.com>
2025-01-22 14:51:11 +0100

    webstore/models.py, webstore/admin.py

--- webstore/admin.py
+++ webstore/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Brand, Product
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "category", "brand", "price",  "quantity", "uploaded_at"]
+    list_filter = ["category", "brand", "price", "uploaded_at"]
+    search_fields = ["name", "brand", "category"]
+    readonly_fields = ["id", "uploaded_at"]
+    
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product, ProductAdmin)
--- webstore/models.py
+++ webstore/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(upload_to="brand_images")
+    
+    def __str__(self):
+        return self.name
+
+class ImageFiled(models.Model):
+    image = models.ImageField(upload_to="product_images")
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    display_image = models.ImageField(upload_to="product_display_images")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    # images = models.ManyToManyField(ImageFiled)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-uploaded_at']
+
+    def __str__(self):
+        return self.name
+
+
+class CartItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def cost(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem, related_name="carts")
+
+    @property
+    def total_cost(self):
+        return sum(item.cost for item in self.items.all())
+
+    def __str__(self):
+        return self.user.username
+
+
+class Order()
